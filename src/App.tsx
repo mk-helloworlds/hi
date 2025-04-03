bb00043e85e8019617f8d2510c6419259899df82
MK <mk@Narys-MacBook-Pro.local>
2025-04-03 23:29:59 +0700

    change bg

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   console.log("Hello my name is MK");
+  console.log("Try to find out more about me")
   return (
     <>
       <SpeedInsights />
       <Analytics />
-      <div className="w-screen h-screen flex justify-center items-center bg-red-50">
+      <div className="w-screen h-screen flex justify-center items-center">
         <ASCIIText text="hi" enableWaves={true} asciiFontSize={8} />
       </div>
     </>
