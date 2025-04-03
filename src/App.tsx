00e50796109a4229173f07a502e4e2d2f346b62f
MK <mk@Narys-MacBook-Pro.local>
2025-04-03 23:25:52 +0700

    analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ASCIIText from "./components/ASCIIText";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   console.log("Hello my name is MK");
   return (
     <>
+      <SpeedInsights />
       <div className="w-screen h-screen flex justify-center items-center bg-red-50">
         <ASCIIText text="hi" enableWaves={true} asciiFontSize={8} />
       </div>
